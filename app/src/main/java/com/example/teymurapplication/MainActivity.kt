bc4072c880d2cf108e22bd45a80c4aec204a94d0
t3ymuurr <aliyevteymur223@gmail.com>
2025-09-27 01:45:32 +0300

    Step 9: Add second Button to change text color

--- app/src/main/java/com/example/teymurapplication/MainActivity.kt
+++ app/src/main/java/com/example/teymurapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.teymurapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val textView: TextView = findViewById(R.id.textView)
-        val button: Button = findViewById(R.id.button)
+        val button1: Button = findViewById(R.id.button1)
+        val button2: Button = findViewById(R.id.button2)
 
-        button.setOnClickListener {
-            textView.text = "Button clicked!"
+        // Button 1 changes text
+        button1.setOnClickListener {
+            textView.text = "Text changed!"
+        }
+
+        // Button 2 changes text color
+        button2.setOnClickListener {
+            textView.setTextColor(Color.RED)
         }
     }
 }
