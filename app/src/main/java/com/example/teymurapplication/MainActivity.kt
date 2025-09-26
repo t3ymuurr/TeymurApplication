e89c01f654992bc21ac956fe4c9e5b092b73d346
t3ymuurr <aliyevteymur223@gmail.com>
2025-09-27 01:35:00 +0300

    Step X: description of changes

--- app/src/main/java/com/example/teymurapplication/MainActivity.kt
+++ app/src/main/java/com/example/teymurapplication/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.teymurapplication
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val textView: TextView = findViewById(R.id.textView)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            textView.text = "Button clicked!"
         }
     }
 }
